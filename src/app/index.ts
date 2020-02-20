030eb4b6268481a5165d70222308f12ffe805afb
SaltyAom <aomkirby@mystiar.com>
2020-02-20 15:20:11 +0700

    :tada: Add more coverage

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 
-import FileService from '@services/file'
 import AppController from './app.controller'
 import AppService from './app.service'
 
 @Module({
     controllers: [AppController],
-    providers: [AppService, FileService],
+    providers: [AppService],
 })
 export default class AppMoudle {}
